7011c2cba4694046af9af6b8f5d301d0cf1de17c
Uladzislau <uladzislau.dzeinichenka@gmail.com>
2022-04-22 15:10:30 +0300

    Lesson 18

--- test/test.test.js
+++ test/test.test.js
@@ -1,45 +1,43 @@
 const { Builder, By, until } = require('selenium-webdriver');
 const { expect } = require('chai');
+const MainPage = require('../pageObject/mainPage');
+const BaseElements = require('../helpers/baseElements');
+const SearchComponent = require('../pageObject/pageComponents/searchComponent');
+const BasePage = require('../pageObject/basePage');
 
-const driver = new Builder().forBrowser('chrome').build();
-driver.manage().window().maximize();
+const mainPage = new MainPage();
+const baseElements = new BaseElements();
+const searchComponent = new SearchComponent();
 
-describe('ChromeDriver Test', function () {
+describe('ChromeDriver Test', () => {
 
-    it('Check title "ChromeDriver"', async () => {
-        await driver.get('https://chromedriver.chromium.org/home');
-        await driver.sleep(3000);
-        const siteTitle = await driver.getTitle();
-        expect(siteTitle).to.include('ChromeDriver');
+    after(async() => {
+        await BasePage.close();
+    });
+
+    it(`Check title "ChromeDriver"`, async () => {
+        await BasePage.navigate('https://chromedriver.chromium.org/home');
+        await driver.wait(until.elementIsVisible(mainPage.homeTitle), 5000);
+        const siteTtile = await mainPage.homeTitle;
+        expect(await siteTtile.getText()).to.contain('ChromeDriver');
     })
 
 
-    it('Check title "Chrome Extencions"', async() => {
-        await driver.get('https://chromedriver.chromium.org/home');
-        await driver.sleep(3000);
-        const chromeExtencions = await driver.findElement(By.xpath('//ul[@jsname = "waIgnc"]//a[text() = "Chrome Extensions"]'));
-        await chromeExtencions.click();
-        await driver.sleep(3000);
-        const siteTitle = await driver.getTitle();
-        expect(siteTitle).to.include('Chrome Extensions');
-
+    it(`Check title "Chrome Extencions"`, async () => {
+        await BasePage.navigate('https://chromedriver.chromium.org/extensions');
+        await driver.wait(until.elementIsVisible(mainPage.homeTitle), 5000);
+        const siteTitle = await mainPage.homeTitle;
+        expect(await siteTitle.getText()).to.contain('Chrome Extensions');
     })
-    
-    it('Check search', async () => {
-        await driver.get('https://chromedriver.chromium.org/home');
-        await driver.sleep(3000);
-        const searchButton = await driver.findElement(By.xpath('//div[@aria-label = "Открыть поле поиска"]'));
-        await searchButton.click();
-        const searchText = await driver.findElement(By.xpath('//input[@class="whsOnd zHQkBf"]'));
-        await driver.wait(until.elementIsVisible(searchText), 3000)
-        await searchText.sendKeys('driver');
-        const startSearchText = await driver.findElement(By.xpath('//div[contains(@class, "U26fgb mUbCce fKz7Od i3PoXe")]/span/span'));
-        await startSearchText.click();
-        await driver.sleep(3000);
-        const firstResult = await driver.findElement(By.xpath('//div[contains(@class, "gtazFe")][1]/div/div'));
-        expect(await firstResult.getText()).to.contain('driver');  
+
+    it('Check Search Results', async () => {
+        await BasePage.navigate('https://chromedriver.chromium.org/');
+        await baseElements.click(searchComponent.searchButton);
+        await baseElements.sendKeys(searchComponent.searchText, 'driver');
+        await baseElements.click(searchComponent.startSearchText);
+        await driver.sleep(2000);
+        await driver.wait(until.elementIsVisible(searchComponent.resultsOnThisSite), 5000);
+        const descriptions = await searchComponent.searchResultDescription;
+        expect(await descriptions.getText()).to.contain('driver');
     })
-})
-
-
- 
+});
